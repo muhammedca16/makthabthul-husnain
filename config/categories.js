098160f21cfbee5a4c021bc906b6ed167d630276
muhammed ca <muhammedmca6@gmail.com>
2026-01-05 10:11:03 +0530

    chenged user interface 2

--- config/categories.js
+++ config/categories.js
@@ -8,5 +8,6 @@ module.exports = [
     { name: 'علم الحديث', link: '/category/علم الحديث' },
     { name: 'ديوان', link: '/category/ديوان' },
     { name: 'الأدب', link: '/category/الأدب' },
-    { name: 'اللغة وعلومها', link: '/category/اللغة وعلومها' }
+    { name: 'اللغة وعلومها', link: '/category/اللغة وعلومها' },
+    { name: 'اللغة وعلومها', link: '/category/الفقه' }
 ];
